992b821e3202667c4e65828f8df8138e2038680d
mlmar <marcloussm@gmail.com>
2026-02-12 01:15:20 -0800

    Moving index.ts to server directory root

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import newsRouter from './api/NewsRoutes.js';
+import newsRouter from './src/api/NewsRoutes.js';
 
 const app = express();
 const port = 3300;
