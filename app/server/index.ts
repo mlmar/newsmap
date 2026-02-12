cd1100687d206949f0a289bd8e175d1dafdfe637
mlmar <marcloussm@gmail.com>
2026-02-12 09:35:19 -0800

    Fixing import paths for ts file

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import newsRouter from './src/api/NewsRoutes';
+import newsRouter from './src/api/NewsRoutes.js';
 
 dotenv.config();
 const app = express();
