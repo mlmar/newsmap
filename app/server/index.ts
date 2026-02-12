35e895d1b7caec695476facb20fdf4e6360d2808
mlmar <marcloussm@gmail.com>
2026-02-12 01:28:02 -0800

    Updating package start script

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import newsRouter from './src/api/NewsRoutes.js';
+import newsRouter from './src/api/NewsRoutes.ts';
 
 dotenv.config();
 const app = express();
