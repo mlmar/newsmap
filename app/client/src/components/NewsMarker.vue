796f7c954186d79ab5a7d29824c56b264e609f3f
mlmar <marcloussm@gmail.com>
2026-02-17 17:06:15 -0800

    #7: Saving last location index on marker click

--- app/client/src/components/NewsMarker.vue
+++ app/client/src/components/NewsMarker.vue
@@ -1,22 +1,32 @@
 <script setup lang="ts">
 import NewsContent from '@/components/NewsContent.vue';
+import { useMapControls } from '@/composables/useMapControl';
+import { useMapData } from '@/composables/useMapData';
 import { useVisibleMapData } from '@/composables/useVisibleMapData';
 import type { MarkerInstance } from '@/types/MarkerInstance';
 import type { MapData } from '@newsmap/types';
 import { LMarker, LPopup } from '@vue-leaflet/vue-leaflet';
 
 const props = defineProps<MapData>();
+const { lastLocationIndex } = useMapControls();
+const { data } = useMapData();
 const { markerRefs } = useVisibleMapData();
+
+function handleMarkerClick() {
+    // Find location index by coordinates
+    lastLocationIndex.value = data.value.findIndex((item) => item.coordinates === props.coordinates);
+}
 </script>
 
 <template>
     <LMarker
-        :lat-lng="coordinates"
+        :lat-lng="props.coordinates"
         :ref="
             (el) => {
-                if (el) markerRefs.set(coordinates.join(), el as MarkerInstance);
+                if (el) markerRefs.set(props.coordinates.join(), el as MarkerInstance);
             }
         "
+        @click="handleMarkerClick"
     >
         <LPopup>
             <NewsContent v-bind="props" />
