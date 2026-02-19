36e2d5f3a66bbb8ad7d3ac37e0d652afdce4a7e4
mlmar <marcloussm@gmail.com>
2026-02-19 10:12:05 -0800

    Updating cache headers

--- app/server/src/api/NewsRoutes.ts
+++ app/server/src/api/NewsRoutes.ts
@@ -11,7 +11,11 @@ router.get('/', async (req, res) => {
     console.log('Fetching news');
     const news = await NewsService.get();
     console.log(`Successfully fetched ${news.length} results`)
-    res.setHeader('Cache-Control', 'public, max-age=3600');
+
+    // s-maxage=3600: Vercel caches this for 1 hour
+    // max-age=0: The browser must check Vercel for updates every time
+    // stale-while-revalidate: Serve old data while Vercel fetches new data in the background
+    res.setHeader('Cache-Control', 'public, s-maxage=3600, max-age=0, stale-while-revalidate=600');
     res.send(news);
 });
 
